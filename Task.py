81c85495df1fe6a79c688135795453b418474ff7
AndreyLogvinovich <Hitri1ksh@gmail.com>
2023-03-17 16:24:46 +0300

    Добавил условие второй задачи

--- Task.py
+++ Task.py
@@ -8,17 +8,17 @@
 """
 def rhythm(str):
     str = str.split()
-    list_1 = []
+    list1 = []
     for word in str:
         sum_w = 0
         for i in word:
             if i in 'аеёиоуыэюя':
                 sum_w += 1
-        list_1.append(sum_w)
-    return len(list_1) == list_1.count(list_1[0])
+        list1.append(sum_w)
+    return len(list1) == list1.count(list1[0])
 
-str_1 = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
-if rhythm(str_1):
+str1 = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
+if rhythm(str1):
     print('Парам пам-пам')
 else:
     print('Пам парам')
